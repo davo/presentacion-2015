2b4f47cdad69044d5e013c336d516f0742fa0f70
Nicolas Lound <nicolas.lound@gmail.com>
2014-12-18 12:31:36 -0300

    separo funcion de video del index

--- js/app.js
+++ js/app.js
@@ -203,8 +203,29 @@ $('#odd').on('change', function () {
 
 });
 
-// $(document).ready(function() {
-//     $('#pagepiling').pagepiling({
-//         sectionSelector: 'section',
-//     });
-// });
+
+
+// Maneja play pause video
+$(document).ready(function() {
+    var media = $('video').not("[autoplay='autoplay']");
+    var tolerancePixel = 40; // le da 40 pixeles de changüí
+
+    function checkMedia(){
+        var scrollTop = $(window).scrollTop() + tolerancePixel;
+        var scrollBottom = $(window).scrollTop() + $(window).height() - tolerancePixel;
+
+        media.each(function(index, el) {
+            var yTopMedia = $(this).offset().top;
+            var yBottomMedia = $(this).height() + yTopMedia;
+
+            if(scrollTop < yBottomMedia && scrollBottom > yTopMedia){
+                $(this).get(0).play();
+            } else {
+                $(this).get(0).pause();
+            }
+        });
+
+        //}
+    }
+    $(document).on('scroll', checkMedia);
+});
